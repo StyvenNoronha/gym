a9dca403ce6379ab0192002b18e5f78f48335adf
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-04-16 08:10:35 -0300

    Hash da senha e validação

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,8 +1,9 @@
 import { prisma } from "@/db/prisma";
 import { z } from "zod";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { hash } from "bcryptjs";
 
-export async function register(request: FastifyRequest, reply:FastifyReply) {
+export async function register(request: FastifyRequest, reply: FastifyReply) {
   const bodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
@@ -10,11 +11,23 @@ export async function register(request: FastifyRequest, reply:FastifyReply) {
   });
   const { name, email, password } = bodySchema.parse(request.body);
 
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where:{
+      email
+    }
+  })
+
+  if(userWithSameEmail){
+    return reply.status(409).send()
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   });
   return reply.status(201).send;

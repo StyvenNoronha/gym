c8435ee13f8d032f2fbbf312deac77b3afc5ad5d
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-04-16 07:59:20 -0300

    controller de registro

---
+++ src/http/controllers/register.controller.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@/db/prisma";
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+
+export async function register(request: FastifyRequest, reply:FastifyReply) {
+  const bodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+  const { name, email, password } = bodySchema.parse(request.body);
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  });
+  return reply.status(201).send;
+}
